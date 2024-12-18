c56a005356332bd4f2ef666a2e1541b5e59b53a7
Luca Capriati <lucapriati246@gmail.com>
2024-12-18 17:00:03 +0200

    Update authOptions.js

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,6 +1,6 @@
 import GoogleProvider from 'next-auth/providers/google';
 import connectDB from "@/config/database";
-import User from "@/models/user";
+import User from '@/models/User';
 
 export const authOptions = {
     providers: [
@@ -19,22 +19,22 @@ export const authOptions = {
     ],
     callbacks: {
         // Invoked on successful sign in
-        async signIn({profile}) {
+        async signIn({ profile }) {
             // 1. Connect to the database
             await connectDB();
 
             // 2. Check if user exists
-            const userExists = await User.findOne({email: profile.email})
+            const userExists = await User.findOne({ email: profile.email })
 
             // 3. If not, then create user
             if (!userExists) {
                 // Truncate username if too long
                 const username = profile.name.slice(0, 20);
-                 await User.create({
-                     email: profile.email,
-                     username: username,
-                     image: profile.picture
-                 })
+                await User.create({
+                    email: profile.email,
+                    username: username,
+                    image: profile.picture
+                })
             }
 
             // 4. Return true to allow sign in
@@ -43,7 +43,7 @@ export const authOptions = {
         // Session callback function that modifies the session object
         async session({ session }) {
             // 1. Get user from database
-            const user = await User.findOne({email: session.user.email})
+            const user = await User.findOne({ email: session.user.email })
 
             // 2. Assign user id from the session
             session.user.id = user._id.toString();
